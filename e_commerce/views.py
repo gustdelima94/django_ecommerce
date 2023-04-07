d0ea7eada6ec8176a875b035be04e9f6039df85a
Gustavo de Lima <gustdelima94@gmail.com>
2023-04-07 13:06:13 -0300

    create pages about and contact

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -2,4 +2,22 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home_Page.html', {})
+    context = {
+        'title': 'O Bon Vivant',
+        'content': 'Bem-vindo a página inicial!'
+    }
+    return render(request, 'home_Page.html', context)
+
+def about_page(request):
+    context = {
+        'title': 'Sobre O Bon Vivant',
+        'content': 'Bem-vindo a página sobre!'
+    }
+    return render(request, 'about/view.html', context)
+
+def contact_page(request):
+    context = {
+        'title': 'Contato O Bon Vivant',
+        'content': 'Bem-vindo a página contato!'
+    }
+    return render(request, 'contact/view.html', context)
