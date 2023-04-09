fb69ce888cd484fad7eef551c1a1579963f6e3d9
Gustavo de Lima <gustdelima94@gmail.com>
2023-04-09 19:40:34 -0300

    rename field description in src/products/models.py and laytou page products/templates/list.html

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=120)
-    desription = models.TextField()
+    description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
 
     def __str__(self):
