d6d215f7b82d34350181aae0cac42c20ade88e0a
Gustavo de Lima <gustdelima94@gmail.com>
2023-04-09 13:46:00 -0300

    create Product class in src/products/models.py

--- products/models.py
+++ products/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    ...
+    title = models.CharField(max_length=120)
+    desription = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
