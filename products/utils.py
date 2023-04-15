299c8b70cc6e6532e99126daba72bc3ade10ecf0
Gustavo de Lima <gustdelima94@gmail.com>
2023-04-15 16:21:31 -0300

    refactor src/products/utils.py

--- products/utils.py
+++ products/utils.py
@@ -19,11 +19,11 @@ def unique_slug_generator(instance, new_slug=None):
         slug = slugify(instance.title)
 
     Klass = instance.__class__
-    qs_exists = Klass.objects.filter(slug = slug).exists()
+    qs_exists = Klass.objects.filter(slug=slug).exists()
     if qs_exists:
         new_slug = "{slug}-{randstr}".format(
                     slug = slug,
                     randstr = random_string_generator(size = 4)
                 )
-        return unique_slug_generator(instance, new_slug = new_slug)
+        return unique_slug_generator(instance, new_slug=new_slug)
     return slug
