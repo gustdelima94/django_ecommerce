96643f46fbe45d25b5e8fbfd93ef1ec68923b9ca
Gustavo de Lima <gustdelima94@gmail.com>
2023-04-15 13:27:29 -0300

    insert slug field in src/products/models.py

--- products/models.py
+++ products/models.py
@@ -11,12 +11,12 @@ class ProductManager(models.Manager):
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField()
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
 
     objects = ProductManager()
-
 
     def __str__(self):
         return self.title
